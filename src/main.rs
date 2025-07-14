48897495dfe5b9b86b1da9b4c0791003f53f44f4
Alan Rostem <42648988+AlanRostem@users.noreply.github.com>
2025-07-14 20:03:48 +0200

    Generic stack implementation.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,12 @@
-use std;
-use std::fmt;
-
-
-struct MyArr {
-    pub data: [i32; 4]
-}
+mod stack;
 
 fn main() {
-    let other: MyArr = MyArr { data: [1, 2, 3, 4] };
-    println!("Hello {}", other);
+    let mut s = stack::Stack::<i32, 4>::new();
+    let _ = s.push(1);
+    _ = s.push(2);
+    _ = s.push(3);
+    _ = s.push(4);
+    println!("Hello {}", s);
 }
 
-impl fmt::Display for MyArr {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut res = write!(f, "[");
-        res.expect("Failed to print opening bracket");
-        for (i, elem) in self.data.iter().enumerate() {
-            res = write!(f, "{}", elem);
-            res.expect("Failed to write number");
-            if i != self.data.len()-1 {
-                res = write!(f, ", ");
-                res.expect("Failed to write comma")
-            }
-        }
-        res = write!(f, "]");
-        res.expect("Failed to print closing bracket");
-        return fmt::Result::Ok(());
-    }
-}
+
--- src/stack.rs
+++ src/stack.rs
@@ -1,5 +1,41 @@
+use std::fmt;
 
-struct Stack<T, const SIZE: usize> {
-    data: [T; SIZE],
+pub struct Stack<T: fmt::Display, const SIZE: usize> {
+    data: [Option<T>; SIZE],
     pointer: usize,
+}
+
+impl<T: fmt::Display, const SIZE: usize> Stack<T, SIZE> {
+    pub fn new() -> Self {
+        return Self{
+            data: [(); SIZE].map(|_| None),
+            pointer: 0,
+        };
+    }
+
+    pub fn push(&mut self, value: T) -> Result<(), &'static str> {
+        if self.pointer == SIZE {
+            return Err("stack overflow");
+        }
+        self.data[self.pointer] = Some(value);
+        self.pointer += 1;
+        Ok(())
+    }
+
+    // TODO: implement pop
+}
+
+impl<T: fmt::Display, const SIZE: usize> fmt::Display for Stack<T, SIZE> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "[")?;
+        for i in 0..self.data.len() {
+            let elem = self.data[i].as_ref().unwrap();
+            write!(f, "{}", elem)?;
+            if i != self.data.len()-1 {
+                write!(f, ", ")?;
+            }
+        }
+        write!(f, "]")?;
+        return fmt::Result::Ok(());
+    }
 }
